7f8308c303318a1e318cb505b187916497a2a832
Garticm2 <nurlanovdias11@gmail.com>
2025-07-23 00:30:34 +0500

    Update 7514596528680867.user.js

--- dist/7514596528680867.user.js
+++ dist/7514596528680867.user.js
@@ -12,15 +12,15 @@
 // @connect     github.io
 // @connect     localhost 
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=garticphone.com
-// @downloadURL http://localhost:8000/userscripts/dist/1591521169568152.user.js
+// @downloadURL https://garticm2.github.io/userscripts/dist/1591521169568152.user.js
 // ==/UserScript==
 
 'use strict';
 
 (function () {
-    const SCRIPT_DATA = { name: "Extensions", version: "4.6", url: "http://localhost:8000/userscripts/dist/1591521169568152.user.js" },
+    const SCRIPT_DATA = { name: "Extensions", version: "4.6", url: "https://garticm2.github.io/userscripts/dist/1591521169568152.user.js" },
         REQUEST_CACHE = { VALIDATE: "no-cache", NO_CACHE: "no-store", DEFAULT: "" },
-        BASE_URL = "http://localhost:8000/pub",
+        BASE_URL = "https://garticm2.github.io/pub",
         resources = { style: ["dist/main.min.css"], script: ["dist/main.min.js"] };
     let inProgress = !1;
     document.addEventListener("DOMContentLoaded", loadResources);
@@ -51,7 +51,7 @@
             inProgress = !0
         }
     }
-    const UPDATES_URL = "http://localhost:8000/userscripts/dist/versions.json";
+    const UPDATES_URL = "https://garticm2.github.io/userscripts/dist/versions.json";
     let outdatedScripts = new Map,
         updatesData = {};
     document.addEventListener("_check-for-updates", () => { document.dispatchEvent(new CustomEvent("_us_check-for-updates", { detail: { ...SCRIPT_DATA } })) });
